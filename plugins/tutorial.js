93f6ea2cf3afaa56ff52f0e995a47fc9fe86e31c
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-27 12:41:31 -0600

    Update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -1,27 +1,27 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-Tutorial Buat Bot WA:
-Credit: *~Drawl Nag*
+Tutorial Para Bot WA:
+Credit: *Susi*
 
-Ketik di *[TERMUX]*
+Escribir *[TERMUX]*
 1) $pkg install git
 2) $pkg install ffmpeg
 3) $pkg install imagemagick
 4) $pkg install nodejs
 
-Untuk Githubnya..
-5) $git clone https://github.com/Arya274/Arya-BOT4
+Para GitHub...
+5) $git clone No esta disponible en estos momentos
 6) $ls
-7) $cd Arya-BOT4
+7) $cd SusiBOT
 
-yang *terakhirr*..
-Penginstalannya cuy :)
+Lo *ultimo*..
+Para instalarlo :)
 8) npm i
 9) node index.js
 
-_Bisa kalian liat tutorialnya di_
-*Youtube kami:*
-https://www.youtube.com/c/DrawlNag
+_Escucha mi canción favorita mejor XD_
+*Youtube:*
+https://youtu.be/KMiDW0g2U0o
 `.trim(), m)
 }
 handler.help = ['tutorial']
